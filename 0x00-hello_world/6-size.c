914cde27bf99d7d896e3df672861f9ef2f568f48
audreysaru <saruaudrey@gmail.com>
2023-10-05 06:08:18 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,23 +6,16 @@
  * Return: 0 when successful
  */
 
-int main(void)
-{
-	printf("Size of a char: 1 byte(s)", sizeof(char));
-	printf("Size of an int: 4 byte(s)", sizeof(int));
-	printf("Size of a long int: 4 byte(s)", sizeof(long int));
-	printf("Size of a long long int: 8 byte(s)", sizeof(long int));
-	printf("Size of a float: 4 byte(s)", sizeof(float));
-	return (0);
-	;
-	int main(void)
-{
-        printf("Size of a char: 1 byte(s)", sizeof(char));
-        printf("Size of an int: 4 byte(s)", sizeof(int));
-        printf("Size of a long int: 8 byte(s)", sizeof(long int));
-        printf("Size of a long long int: 8 byte(s)", sizeof(long int));
-        printf("Size of a float: 4 byte(s)", sizeof(float));
-        return (0);
-        ;
-}
+int main() {
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of double: %zu bytes\n", sizeof(doubleType));
+    printf("Size of char: %zu byte\n", sizeof(charType));
+    
+    return 0;
 }
