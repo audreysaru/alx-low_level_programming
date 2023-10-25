f8a78f613945b0aabe506e894ec6b309b6e0cd1b
audreysaru <saruaudrey@gmail.com>
2023-10-24 18:58:45 -0700

    Even More Pointers Arrays and Strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,22 +7,47 @@
  * Return: needle or NULL
  */
 
+#include <stdio.h>
+
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-
-	while (haystack[i] != '\0')
+	while (*haystack)
 	{
-		j = 0;
-		while (needle[j] != '\0' && haystack[i + j] == needle[j])
-		{
-			j++;
-		}
-		if (needle[j] == '\0')
-		{
-			return (&haystack[i]);
-		}
-		i++;
+	char *start = haystack;
+	char *sub = needle;
+
+	while (*haystack && *sub && *haystack == *sub)
+	{
+		haystack++;
+		sub++;
 	}
+
+	if (!*sub)
+	{
+		return (start);
+	}
+	haystack = start + 1;
+	}
+
 	return (NULL);
 }
+
+int main()
+{
+	char haystack[] = "Hello, this is a sample string.";
+	char needle[] = "is";
+
+	char *result = _strstr(haystack, needle);
+
+	if (result)
+	{
+	printf("Substring found: %s\n", result);
+	}
+	else
+	{
+	printf("Substring not found.\n");
+	}
+
+	return (0);
+}
+
