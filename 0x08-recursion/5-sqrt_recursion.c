ec92acfb9657af12668dec3c631d3b4524223a34
audreysaru <saruaudrey@gmail.com>
2023-10-25 20:27:59 -0700

    C Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,37 @@
 /**
  * _sqrt_recursion - returns square  root of a number
  * @n: number
- * Return: int
+ * Return: square root of
  */
 
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return (find_sqrt(n, 0));
+}
+
+/**
+ * find_sqrt - Helper function to find the square root recursively
+ * @n: The number to find the square root of
+ * @i: The current value to check
+ * Return: The square root if found, -1 if not
+ */
+
+int find_sqrt(int n, int i)
+{
+	if (i * i == n)
+	{
+	return (i);
+	}
+
+	if (i * i > n)
+	{
+	return (-1);
+	}
+
+	return (find_sqrt(n, i + 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,9 +3,42 @@
 /**
  * is_prime_number - returns prime numbers
  * @n: number
+ * @i: variable
  * Return: int
  */
 
-int is_prime_number(int n)
+int is_prime_number(int n, int i)
 {
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (n <= 3)
+	{
+		return (1);
+	}
+
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+
+	if (n % i == 0 || n % (i + 2) == 0)
+	{
+		return (0);
+	}
+
+	/**
+	* while (i * i <= n)
+	* {
+	*	if (n % i == 0 || n % (i + 2) == 0)
+	*	{
+	*		return (0);
+	*	}
+	* i += 6;
+	* }
+	*/
+
+	return (1);
 }
