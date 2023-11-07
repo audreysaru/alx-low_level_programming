f324f9a620d0cbfe955a7b678c83a9871c78cc45
audreysaru <saruaudrey@gmail.com>
2023-11-06 18:02:31 -0800

    C Structures typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - intializes a dog
+ * @d: variable type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,72 @@
+#include "dog.h"
+
+/**
+ * _strlen - string length
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = _strlen(src);
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - create a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	ndog->name = malloc(sizeof(char) * (len1 + 1));
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	_strcpy(ndog->name, name);
+	_strcpy(ndog->owner, owner);
+	ndog->age = age;
+	return (ndog);
+}
